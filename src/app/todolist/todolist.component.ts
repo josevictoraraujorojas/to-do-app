7659005bec4b3fabb474600a7083178f83419822
josevictoraraujorojas <kaneki1212112121@gmail.com>
2025-01-28 08:09:05 -0300

    editar

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -9,14 +9,15 @@ import { NgForm } from '@angular/forms';
 })
 export class TodolistComponent {
 
-  taskArray = [{taskName: 'Arroz', isCompleted: false}];
+  taskArray = [{taskName: 'Arroz', isCompleted: false, isEditable:false}];
 
   onSubmit(form: NgForm){
     console.log(form);
 
     this.taskArray.push({
       taskName: form.controls['task'].value,
-      isCompleted: false
+      isCompleted: false,
+      isEditable:false
     })
     form.reset
   }
@@ -24,6 +25,15 @@ export class TodolistComponent {
   onDelet(index: number){
     console.log(index);
     this.taskArray.splice(index,1);
+  }
+  
+  onEdit(index: number){
+    this.taskArray[index].isEditable=true;
+  }
+
+  onSave(index: number, newtask: string){
+    this.taskArray[index].taskName=newtask;
+    this.taskArray[index].isEditable=false;
   }
 
   onCheck(index:number){
